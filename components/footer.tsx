9ac4925b80e31052a2dadbb696cb519888c60f3c
sh4lluloveme <wh3n_gg@kakao.com>
2023-11-14 23:50:34 +0900

    Edit: edit pages

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => (
+  <div className="flex flex-col items-center">
+    <div className="flex">
+      <h4 className="text-white text-xs mr-4">개인정보처리방침</h4>
+      <h4 className="text-white text-xs mr-4">이용약관</h4>
+      <h4 className="text-white text-xs mr-4">팀 정보</h4>
+    </div>
+  </div>
+);
+
+export default Footer;
