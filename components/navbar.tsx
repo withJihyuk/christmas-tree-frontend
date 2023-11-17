a135190d154a6ac741cc7ecda3669c41acb26264
sh4lluloveme <wh3n_gg@kakao.com>
2023-11-18 03:09:57 +0900

    Edit navbar icon and function

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 export function Navbar() {
+  const router = useRouter();
+
   return (
     <header>
       <div className="absolute left-0 m-4">
@@ -9,6 +14,7 @@ export function Navbar() {
           viewBox="0 0 1024 1024"
           xmlns="http://www.w3.org/2000/svg"
           fill="#ffffff"
+          onClick={() => router.back()}
           stroke="#ffffff"
         >
           <g id="SVGRepo_bgCarrier" strokeWidth="0" />
@@ -33,7 +39,7 @@ export function Navbar() {
         </svg>
       </div>
       <div className="absolute right-0 m-4">
-        <svg
+        {/* <svg
           className="w-8 h-8 text-blue-500"
           width="800px"
           height="800px"
@@ -57,6 +63,39 @@ export function Navbar() {
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
+            />{" "}
+          </g>
+        </svg> */}
+        <svg
+          className="w-8 h-8 text-blue-500"
+          width="800px"
+          height="800px"
+          viewBox="0 0 24 24"
+          fill="none"
+          onClick={() => router.push("/")}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M22 12.2039V13.725C22 17.6258 22 19.5763 20.8284 20.7881C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.7881C2 19.5763 2 17.6258 2 13.725V12.2039C2 9.91549 2 8.77128 2.5192 7.82274C3.0384 6.87421 3.98695 6.28551 5.88403 5.10813L7.88403 3.86687C9.88939 2.62229 10.8921 2 12 2C13.1079 2 14.1106 2.62229 16.116 3.86687L18.116 5.10812C20.0131 6.28551 20.9616 6.87421 21.4808 7.82274"
+              stroke="#ffffff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            />{" "}
+            <path
+              d="M15 18H9"
+              stroke="#ffffff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
             />{" "}
           </g>
         </svg>
